90b61f9ceff49603ea3631fdd5692c5c2ada1d28
The-Minecraft-Scientist <charlesliske@gmail.com>
2023-12-18 18:45:26 -0800

    pretty Debug impl for EquationType

--- src/ast/expression.rs
+++ src/ast/expression.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use thin_vec::ThinVec;
 
 use super::{parse_manager::AST, ASTNode, ASTNodeRef, Comparison, Ident, Opcode};
@@ -30,7 +31,7 @@ impl<'a> ExpressionMeta<'a> {
     };
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum ExpressionType<'a> {
     Fn {
         name: Ident<'a>,
@@ -41,6 +42,24 @@ pub enum ExpressionType<'a> {
         eq_type: EquationType,
     },
 }
+impl<'a> Debug for ExpressionType<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Fn { name, params } => {
+                let mut s = format!("Fn {}(", name.0.as_str());
+                for p in params {
+                    s.push_str(&p.0);
+                    s.push(',');
+                }
+                let _ = s.pop();
+                f.write_fmt(format_args!("{})", s))
+            }
+            Self::Var(arg0) => f.debug_tuple("Var").field(&arg0.0).finish(),
+            Self::Eq { eq_type } => f.debug_struct("Eq").field("eq_type", eq_type).finish(),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum EquationType {
     Implicit,
