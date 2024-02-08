7355c8949e32ba81e5d77dc2dfb24e38b70cbf08
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2024-02-07 18:00:46 -0800

    idek what I changed

--- src/compile/optimize/egraph.rs
+++ src/compile/optimize/egraph.rs
@@ -1,8 +1,11 @@
 use super::disjoint_set::UsizeDisjointSet;
 
-pub struct EGraph {
+pub struct EGraph<Term> {
     //Union-find datastructure. Maps E-node (operation) Ids to the "canonical" (representative) Id
-    classes: UsizeDisjointSet<Id>,
+    classes: UsizeDisjointSet<Term>,
+}
+pub trait TermIntern<Term> {
+    fn map(t: Term) -> usize
 }
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Id(usize);
